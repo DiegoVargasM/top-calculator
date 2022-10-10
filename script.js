fa5424aeb5eb2d92296be3f1f9139dcd26b297f0
diegoxwo <diegovargasmiranda@gmail.com>
2022-10-10 11:20:23 -0500

    Add event listeners and display actualization

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
+//create global variables for values and operator
+let previousValue = '';
+let currentValue = '';
+let operator = '';
+
 //store all components on HTML in our JS
-let clear = document.querySelector("#clear-btn");
+let clear = document.querySelector(".clear");
+let dlt = document.querySelector(".delete");
 let equal = document.querySelector(".btn-equal");
-let decimal = document.querySelector("btn-decimal");
+let decimal = document.querySelector(".btn-decimal");
 
 let numbers = document.querySelectorAll(".btn-number");
-let operators = decument.querySelectorAll(".btn-o");
+let operators = document.querySelectorAll(".btn-o");
 
 let previousScreen = document.querySelector(".previous");
 let currentScreen = document.querySelector(".current");
+
+//create event listener for number buttons and change currentValue
+numbers.forEach((number) => number.addEventListener('click', function(e) {
+    handleNumber(e.target.value);
+    //show the currentValue in the currentScreen display
+    currentScreen.textContent = currentValue;
+}))
+
+//function to add the clicked numbers to the currentValue variable
+function handleNumber(num) {
+    if (currentValue.length <= 5) {
+        currentValue += num;
+    }
+    console.log(currentValue);
+}
+
+//create event listener for operator buttons and change operator value
+operators.forEach((op) => op.addEventListener('click', function (e) {
+    handleOperator(e.target.value);
+    //after the operator has been chosen, display the prev value and the
+    //operator on the screen
+    previousScreen.textContent = previousValue + " " + operator;
+}))
+
+//function to add the operator to the operator variable, move 
+//the current value to the previous value and then empty the current value
+function handleOperator(op) {
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
+}
